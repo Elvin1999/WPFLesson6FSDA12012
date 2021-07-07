4266bce3005ade012be106c8c7d68a1ebb035298
Elvin <elvincamalzade00@gmail.com>
2021-07-07 16:57:09 +0400

    Add project files.

---
+++ Resources/Info.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for Info.xaml
+    /// </summary>
+    public partial class Info : Window//,INotifyPropertyChanged
+    {
+
+
+
+        public Car InfoCar
+        {
+            get { return (Car)GetValue(InfoCarProperty); }
+            set { SetValue(InfoCarProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InfoCar.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InfoCarProperty =
+            DependencyProperty.Register("InfoCar", typeof(Car), typeof(Info));
+
+
+
+        public Info()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        //private Car car;
+
+        //public Car InfoCar
+        //{
+        //    get { return car; }
+        //    set { car = value; OnPropertyChanged(); }
+        //}
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected void OnPropertyChanged([CallerMemberName] string name = null)
+        //{
+        //    PropertyChangedEventHandler handler = PropertyChanged;
+        //    if (handler != null)
+        //    {
+        //        handler(this, new PropertyChangedEventArgs(name));
+        //    }
+        //}
+    }
+}
---
+++ Resources/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string someText;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string name=null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public string SomeText
+        {
+            get { return someText; }
+            set
+            {
+                someText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Car MyCar { get; set; }
+        public ObservableCollection<Car> Cars { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            //SomeText = "Hakuna";
+
+            MyCar = new Car
+            {
+                Model = "X7",
+                Vendor = "BMW",
+                Year = 2021
+            };
+
+            Cars = new ObservableCollection<Car>
+            {
+                new Car
+                {
+                    Model="X5",
+                    Vendor="BMW",
+                    Year=2020
+                },
+                new Car
+                {
+                    Model="E-200",
+                    Vendor="Mercedes",
+                    Year=2020
+                },
+                new Car
+                {
+                    Model="Cruze 2LT",
+                    Vendor="Chevrolet",
+                    Year=2010
+                },
+            };
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MyCar.Model = "Chevrolet";
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["PrimaryColor"] = Brushes.DeepPink;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+
+            gradientBrush.GradientStops.Add(new GradientStop
+            {
+                Color = Colors.Blue,
+                Offset = 0.0
+            });
+            gradientBrush.GradientStops.Add(new GradientStop
+            {
+                Color = Colors.Yellow,
+                Offset = 1.0
+            });
+
+            this.Resources["SpecialColor"] = gradientBrush;
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var info = new Info();
+            info.InfoCar = (sender as ListBox).SelectedItem as Car;
+            info.ShowDialog();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Car car = new Car
+            {
+                Model = "LaFerrari",
+                Vendor = "Ferrari",
+                Year = 2020
+            };
+            Cars.Add(car);
+        }
+    }
+}
